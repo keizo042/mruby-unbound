8523870e28fd52769b167337e75bbc46ca18f449
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-02-20 23:34:11 +0900

    enum methods for test

--- test/unbound_test.rb
+++ test/unbound_test.rb
@@ -1,2 +1,21 @@
 
-a = Unbound.new 
+unbound = Unbound.new 
+
+unbound.resolvconf 
+unbound.set_option
+unbound.get_option
+unbound.hosts
+unbound.config
+unbound.set_fwd
+unbound.add_ta
+unbound.add_ta_autr
+unbound.add_ta_file
+unbound.trustedkeys
+unbound.debuglevel
+unbound.fd
+unbound.poll
+unbound.process
+unbound.cancel
+unbound.print_local_zones
+unbound.zone_add
+unbound.zone_remove
